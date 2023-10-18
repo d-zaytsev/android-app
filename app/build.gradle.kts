163db05a9890c837d44d48a6df88a4bab10d062e
d-zaytsev <st107687@student.spbu.ru>
2023-10-18 17:29:05 +0300

    Add Jetpack Compose to project

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,16 +5,23 @@ plugins {
 
 android {
     namespace = "com.example.android_app"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.android_app"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+    buildFeatures {
+        compose = true
+    }
+
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.5.2"
     }
 
     buildTypes {
@@ -27,20 +34,30 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
-    }
-    kotlinOptions {
-        jvmTarget = "1.8"
+        sourceCompatibility = JavaVersion.VERSION_18
+        targetCompatibility = JavaVersion.VERSION_18
     }
 }
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.9.0")
+    // ---Compose---
+    implementation("androidx.compose.runtime:runtime:1.6.0-alpha07")
+    implementation("androidx.compose.material3:material3:1.1.2")
+    implementation("androidx.compose.material3:material3-window-size-class:1.1.2")
+    implementation("androidx.activity:activity-ktx:1.8.0")
+    implementation("androidx.activity:activity-compose:1.8.0")
+
+    // Android Studio Preview support
+    implementation("androidx.compose.ui:ui-tooling-preview")
+    debugImplementation("androidx.compose.ui:ui-tooling")
+
+    // ---
+
+    implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.8.0")
+    implementation("com.google.android.material:material:1.10.0")
+
     implementation(project(mapOf("path" to ":musiclib")))
     testImplementation("org.junit.jupiter:junit-jupiter:5.10.0")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 }
