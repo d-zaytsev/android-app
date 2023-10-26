cd9f4d35481fe625184aa0c2b6ae725cdf126d83
d-zaytsev <st107687@student.spbu.ru>
2023-10-26 17:45:21 +0300

    Add new funcs in NoteRange

--- musiclib/src/main/java/com/musiclib/notes/NoteRange.kt
+++ musiclib/src/main/java/com/musiclib/notes/NoteRange.kt
@@ -1,5 +1,27 @@
 package com.musiclib.notes
 
-data class NoteRange(val noteA: Note, val noteB: Note) {
-    fun inRange(note: Note): Boolean = note in noteA..noteB
+/**
+ * Представляет собой определённый диапазон нот
+ */
+data class NoteRange(val from: Note, val to: Note) {
+
+    init {
+        if (from >= to)
+            throw IllegalArgumentException("Left border cannot be greater than or equal to the right")
+    }
+
+    /** Количество нот в диапазоне */
+    val noteCount: Int
+        get() = (to.octave * 7 - to.name.ordinal) - (from.octave * 7 - from.name.ordinal)
+
+    fun inRangeAll(elements: Collection<Note>): Boolean {
+        for (item in elements)
+            if (!inRange(item))
+                return false
+
+        return true
+    }
+
+    /** @return Находится ли переданная нота в диапазоне */
+    fun inRange(element: Note): Boolean = element in from..to
 }
