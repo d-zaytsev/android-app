f19840a91b9ae66d1750f84f46ef9e629e7952ab
dmitriy <st107687@student.spbu.ru>
2023-11-13 10:44:11 +0300

    Delete Note functions from MusicNote interface

--- musiclib/src/main/java/com/musiclib/notes/MusicNote.kt
+++ musiclib/src/main/java/com/musiclib/notes/MusicNote.kt
@@ -13,22 +13,4 @@ interface MusicNote {
 
     /** Номер октавы, 0 - главная */
     val octave: Int;
-
-    /** @return Следующую ноту относительно этой */
-    fun next(): Note;
-
-    /** @return Предыдущую ноту относительно этой */
-    fun previous(): Note;
-
-    /** @return Целая ли нота */
-    fun isWhole(): Boolean
-
-    /** @return Целая нота */
-    fun toWhole(): Note
-
-    /** @return Повышенная на пол тона нота */
-    fun toExt(): Note
-
-    /**@return Пониженная на пол тона нота*/
-    fun loLow(): Note
 }
