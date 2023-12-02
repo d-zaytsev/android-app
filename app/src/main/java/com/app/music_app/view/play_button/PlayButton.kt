9c6e58ebb25c9a2f0e5a45695bc385343238f53a
dmitriy <st107687@student.spbu.ru>
2023-12-02 13:34:35 +0300

    Convert PlayButton to global fun

--- app/src/main/java/com/app/music_app/view/play_button/PlayButton.kt
+++ app/src/main/java/com/app/music_app/view/play_button/PlayButton.kt
@@ -2,11 +2,8 @@ package com.app.music_app.view.play_button
 
 import android.view.MotionEvent
 import androidx.compose.animation.core.animateFloatAsState
-import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.offset
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.QueueMusic
 import androidx.compose.material3.Button
@@ -26,63 +23,58 @@ import androidx.compose.ui.input.pointer.pointerInteropFilter
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.DpSize
-import androidx.compose.ui.unit.TextUnit
-import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.app.music_app.view.colors.AppColors
 import com.example.android_app.R
 
-class PlayButton(private val size: DpSize) {
+@OptIn(ExperimentalComposeUiApi::class)
+@Composable
+fun PlayButton(size: DpSize) {
+    val iconWidth = size.height
+    val textSize = (size.height.value / 2).sp
 
-    private val iconWidth = size.height
-    private val textSize = (size.height.value / 2).sp
+    val text = stringResource(R.string.repeat)
 
-    @OptIn(ExperimentalComposeUiApi::class)
-    @Composable
-    fun Draw() {
-        val text = stringResource(R.string.repeat)
+    var selected by remember { mutableStateOf(false) }
+    val scale by animateFloatAsState(if (selected) 0.8f else 1f, label = "")
 
-        var selected by remember { mutableStateOf(false) }
-        val scale by animateFloatAsState(if (selected) 0.8f else 1f, label = "")
-
-        Button(
-            onClick = { /*TODO*/ },
-            colors = ButtonDefaults.buttonColors(
-                containerColor = AppColors.lightBlue
-            ),
-            modifier = Modifier
-                .size(size)
-                .scale(scale)
-                .pointerInteropFilter {
-                    // рассматриваются разные случаи action
-                    when (it.action) {
-                        MotionEvent.ACTION_DOWN -> {
-                            selected = true
-                        }
-
-                        MotionEvent.ACTION_UP -> {
-                            selected = false
-                        }
+    Button(
+        onClick = { },
+        colors = ButtonDefaults.buttonColors(
+            containerColor = AppColors.lightBlue
+        ),
+        modifier = Modifier
+            .size(size)
+            .scale(scale)
+            .pointerInteropFilter {
+                // рассматриваются разные случаи action
+                when (it.action) {
+                    MotionEvent.ACTION_DOWN -> {
+                        selected = true
                     }
-                    true
+
+                    MotionEvent.ACTION_UP -> {
+                        selected = false
+                    }
                 }
-        ) {
-            Icon(
-                modifier = Modifier
-                    .size(iconWidth),
-                imageVector = Icons.Rounded.QueueMusic,
-                contentDescription = "Refresh button",
-                tint = Color.White
-            )
-            Text(
-                text = text,
-                color = Color.White,
-                textAlign = TextAlign.Center,
-                fontSize = textSize,
-                modifier = Modifier
-                    .weight(1f)
-                    .offset(x = -iconWidth / 2), //иконка сдвигает текст слишком вправо
-            )
-        }
+                true
+            }
+    ) {
+        Icon(
+            modifier = Modifier
+                .size(iconWidth),
+            imageVector = Icons.Rounded.QueueMusic,
+            contentDescription = "Refresh button",
+            tint = Color.White
+        )
+        Text(
+            text = text,
+            color = Color.White,
+            textAlign = TextAlign.Center,
+            fontSize = textSize,
+            modifier = Modifier
+                .weight(1f)
+                .offset(x = -iconWidth / 2), //иконка сдвигает текст слишком вправо
+        )
     }
 }
