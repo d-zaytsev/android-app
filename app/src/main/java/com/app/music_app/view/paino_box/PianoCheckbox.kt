5c4aa010d27df1543a0f210a4929f3aaf63981c6
dmitriy <st107687@student.spbu.ru>
2023-12-10 20:38:28 +0300

    Make boxes clickable

--- app/src/main/java/com/app/music_app/view/paino_box/PianoCheckbox.kt
+++ app/src/main/java/com/app/music_app/view/paino_box/PianoCheckbox.kt
@@ -1,19 +1,35 @@
 package com.app.music_app.view.paino_box
 
+import android.util.Log
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.gestures.awaitEachGesture
+import androidx.compose.foundation.gestures.awaitFirstDown
+import androidx.compose.foundation.gestures.detectDragGestures
+import androidx.compose.foundation.gestures.detectTapGestures
+import androidx.compose.foundation.gestures.forEachGesture
+import androidx.compose.foundation.gestures.waitForUpOrCancellation
+import androidx.compose.foundation.indication
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.ripple.rememberRipple
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
@@ -30,9 +46,11 @@ import com.example.android_app.R
 @Composable
 fun PianoCheckbox(
     modifier: Modifier = Modifier,
-    borderColor: Color = Color.White,
-    pianoBoxBackColor: Color = AppColors.LightCyan,
+    backColor: Color = AppColors.WhiteSmoke,
+    pianoBoxDefaultColor: Color = AppColors.LightCyan,
+    pianoBoxPressedColor: Color = AppColors.PacificCyan,
     textColor: Color = AppColors.PacificCyan,
+    onPianoClick: (PianoKeyboard) -> Unit,
     vararg keyboards: PianoKeyboard,
 ) {
     Column(
@@ -50,25 +68,37 @@ fun PianoCheckbox(
             modifier = Modifier
                 .fillMaxSize()
                 .border(
-                    BorderStroke(3.dp, color = borderColor),
+                    BorderStroke(3.dp, color = backColor),
                     shape = RoundedCornerShape(10.dp)
                 )
-                .background(borderColor, shape = RoundedCornerShape(10.dp)),
+                .background(backColor, RoundedCornerShape(10.dp)),
             contentAlignment = Alignment.Center
         ) {
-            RowWithWrap (horizontalSpacer = 20.dp,
-                verticalSpacer = 20.dp) {
+            RowWithWrap(
+                horizontalSpacer = 20.dp,
+                verticalSpacer = 20.dp
+            ) {
                 // Box с клавиатурой
                 repeat(keyboards.size) {
+                    // Цвет каждого квадрата (меняется при нажатии)
+                    var color by remember { mutableStateOf(pianoBoxDefaultColor) }
+
                     Box(
                         modifier = Modifier
                             .size(keyboards[it].size.times(1.2f))
                             .border(
-                                BorderStroke(1.dp, color = borderColor),
+                                BorderStroke(1.dp, color = color),
                                 shape = RoundedCornerShape(15.dp)
                             )
-                            .background(pianoBoxBackColor, shape = RoundedCornerShape(15.dp))
-                            .padding(3.dp),
+                            .background(color, shape = RoundedCornerShape(15.dp))
+                            .padding(3.dp)
+                            .pointerInput(Unit) {
+                                awaitEachGesture {
+                                    color = pianoBoxPressedColor
+                                    Log.w("TEST", this.currentEvent.type.toString())
+                                    awaitFirstDown().also { it.consume() }
+                                }
+                            },
                         contentAlignment = Alignment.Center
                     ) {
                         // Рисуем клавиатуру внутри каждого квадратика
