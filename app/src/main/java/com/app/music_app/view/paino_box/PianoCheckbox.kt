506a1427dccad77f1dcbd883edc61b9e59edd8e3
dmitriy <st107687@student.spbu.ru>
2023-12-10 21:14:02 +0300

    add restrictions on clicks and information display function

--- app/src/main/java/com/app/music_app/view/paino_box/PianoCheckbox.kt
+++ app/src/main/java/com/app/music_app/view/paino_box/PianoCheckbox.kt
@@ -1,25 +1,14 @@
 package com.app.music_app.view.paino_box
 
-import android.util.Log
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.gestures.awaitEachGesture
-import androidx.compose.foundation.gestures.awaitFirstDown
-import androidx.compose.foundation.gestures.detectDragGestures
-import androidx.compose.foundation.gestures.detectTapGestures
-import androidx.compose.foundation.gestures.forEachGesture
-import androidx.compose.foundation.gestures.waitForUpOrCancellation
-import androidx.compose.foundation.indication
-import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.ripple.rememberRipple
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -29,7 +18,6 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.input.pointer.PointerEventType
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
@@ -51,7 +39,7 @@ fun PianoCheckbox(
     pianoBoxDefaultColor: Color = AppColors.LightCyan,
     pianoBoxPressedColor: Color = AppColors.PacificCyan,
     textColor: Color = AppColors.PacificCyan,
-    onPianoClick: (PianoKeyboard) -> Unit,
+    onPianoClick: (keyboard: PianoKeyboard, isLast: Boolean) -> Unit,
     vararg keyboards: PianoKeyboard,
 ) {
     Column(
@@ -79,6 +67,11 @@ fun PianoCheckbox(
                 horizontalSpacer = 20.dp,
                 verticalSpacer = 20.dp
             ) {
+
+                var clicksCount by remember { mutableStateOf(0) }
+                // Чтобы элемент переставал работать после всех выборов
+                var canClick by remember { mutableStateOf(true) }
+
                 // Box с клавиатурой
                 repeat(keyboards.size) {
                     // Цвет каждого квадрата (меняется при нажатии)
@@ -94,7 +87,14 @@ fun PianoCheckbox(
                             .background(color, shape = RoundedCornerShape(15.dp))
                             .padding(3.dp)
                             .pointerInput(Unit) {
+                                if (canClick) {
                                     color = pianoBoxPressedColor
+                                    clicksCount++
+                                    // Когда остаётся один вариант
+                                    if (clicksCount >= keyboards.size - 1)
+                                        canClick = false
+                                    onPianoClick(keyboards[it], !canClick)
+                                }
                             },
                         contentAlignment = Alignment.Center
                     ) {
