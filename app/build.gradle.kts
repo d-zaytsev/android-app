4edb89eead2edddad05fd284352649c2615790fc
dmitriy <st107687@student.spbu.ru>
2023-12-17 14:07:28 +0300

    Update libraries & add nav

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,13 +46,15 @@ tasks.withType<Test> {
 dependencies {
 
     // ---Compose---
-    implementation("androidx.compose.runtime:runtime:1.6.0-beta02")
+    implementation("androidx.compose.runtime:runtime:1.6.0-beta03")
     implementation ("androidx.compose.material3:material3")
     implementation("androidx.compose.material3:material3-window-size-class:1.1.2")
-    implementation("androidx.activity:activity-ktx:1.8.1")
-    implementation("androidx.activity:activity-compose:1.8.1")
+    implementation("androidx.activity:activity-ktx:1.8.2")
+    implementation("androidx.activity:activity-compose:1.8.2")
     implementation("androidx.compose.material:material:1.5.4")
     implementation("androidx.compose.ui:ui:1.5.4")
+    // Navigation
+    implementation("androidx.navigation:navigation-common:2.7.6")
     // Full set of material icons
     implementation("androidx.compose.material:material-icons-extended")
 
@@ -65,7 +67,7 @@ dependencies {
 
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.10.0")
+    implementation("com.google.android.material:material:1.11.0")
 
     implementation(project(mapOf("path" to ":musiclib")))
     testImplementation("org.junit.jupiter:junit-jupiter:5.10.0")
