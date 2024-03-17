a489121be6c21c2cc78d4d27ac4bfd3636a61e5c
dmitriy <st107687@student.spbu.ru>
2024-03-17 19:54:32 +0300

    Fix: first and last notes were not included in the list

--- musiclib/src/main/java/com/musiclib/notes/range/NoteIterator.kt
+++ musiclib/src/main/java/com/musiclib/notes/range/NoteIterator.kt
@@ -6,9 +6,9 @@ class NoteIterator(start: Note, private val end: Note) : Iterator<Note> {
 
     private var currentNote = start
     override fun hasNext(): Boolean = currentNote <= end
-
     override fun next(): Note {
+        val temp = currentNote
         currentNote = currentNote.nextSemitone()
-        return currentNote
+        return temp
     }
 }
