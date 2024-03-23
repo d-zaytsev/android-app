3bf394f8b8b1c947be34a063a318094d09010a46
dmitriy <st107687@student.spbu.ru>
2024-03-23 17:35:08 +0300

    Gradle: add TarsosDSP rep

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,15 +46,15 @@ tasks.withType<Test> {
 dependencies {
 
     // ---Compose---
-    implementation("androidx.compose.runtime:runtime:1.6.0-beta03")
+    implementation("androidx.compose.runtime:runtime:1.6.4")
     implementation ("androidx.compose.material3:material3")
-    implementation("androidx.compose.material3:material3-window-size-class:1.1.2")
+    implementation("androidx.compose.material3:material3-window-size-class:1.2.1")
     implementation("androidx.activity:activity-ktx:1.8.2")
     implementation("androidx.activity:activity-compose:1.8.2")
-    implementation("androidx.compose.material:material:1.5.4")
-    implementation("androidx.compose.ui:ui:1.5.4")
+    implementation("androidx.compose.material:material:1.6.4")
+    implementation("androidx.compose.ui:ui:1.6.4")
     // Navigation
-    implementation("androidx.navigation:navigation-compose:2.7.6")
+    implementation("androidx.navigation:navigation-compose:2.7.7")
     // Full set of material icons
     implementation("androidx.compose.material:material-icons-extended")
 
@@ -72,5 +72,5 @@ dependencies {
     implementation(project(mapOf("path" to ":musiclib")))
     testImplementation("org.junit.jupiter:junit-jupiter:5.10.0")
     testImplementation(kotlin("test-junit5"))
-    testImplementation(platform("org.junit:junit-bom:5.7.0"))
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
 }
