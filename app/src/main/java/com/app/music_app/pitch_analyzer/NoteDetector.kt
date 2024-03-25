5ba98cbcf75c9b8ea8b737ac50fe16886b411221
dmitriy <st107687@student.spbu.ru>
2024-03-25 20:03:09 +0300

    Refactoring

--- app/src/main/java/com/app/music_app/pitch_analyzer/NoteDetector.kt
+++ app/src/main/java/com/app/music_app/pitch_analyzer/NoteDetector.kt
@@ -8,7 +8,6 @@ import be.tarsos.dsp.pitch.PitchProcessor
 import be.tarsos.dsp.pitch.PitchProcessor.PitchEstimationAlgorithm
 import com.musiclib.notes.Note
 import com.musiclib.notes.note_metadata.NoteName
-import kotlin.concurrent.thread
 import kotlin.math.log
 import kotlin.math.round
 
@@ -25,101 +24,95 @@ class NoteDetector(
     private val pitchEstimationAlgorithm: PitchEstimationAlgorithm = PitchEstimationAlgorithm.FFT_YIN
 ) {
 
-    // ВАЖНО!!! Тут выставлены специальные ограничения на распознаваемые частоты,
-    // сделано это с целью повысить распознаваемость уже имеющихся нот!
+    // # ---------------------------------------------------------------------------- #
+    //  ВАЖНО!!! Тут выставлены специальные ограничения на распознаваемые частоты,
+    //  сделано это с целью повысить распознаваемость уже имеющихся нот!
 
-    // Можно убрать эти границы, но => баги, т.к. звук распадается на другие частоты.
-    // Это можно поправить, но ценой времени и производительности.
+    //  Можно убрать эти границы, но => баги, т.к. звук распадается на другие частоты.
+    //  Это можно поправить, но ценой времени и производительности.
+    // # ---------------------------------------------------------------------------- #
 
-    // Поток, в котором мы слушаем микрофон и распознаём высоты
-    private lateinit var recognizingThread: Thread
+    // Creates AudioDispatcher from configured default microphone,
+    // AudioDispatcher - breaks sounds into float arrays. Needs for pitch detectors, audio players, ...
+    private val dispatcher = AudioDispatcherFactory.fromDefaultMicrophone(
+        sampleRate,
+        audioBufferSize,
+        bufferOverlap
+    )
 
     private companion object {
         const val PROBABILITY_LIMIT: Float = 0.9f
 
-        const val MAIN_HZ: Double = 130.81   // Нота, от которой мы считаем
-        const val MAIN_OCTAVE_SHIFT = -1    // На сколько октав смещена наша нота До
+        const val START_NOTE_HZ: Double = 130.81   // Нота, от которой мы считаем
+        const val OCTAVE_SHIFT = -1                // На сколько октав смещена наша нота До
 
-        const val MIN_HZ: Double = 130.81   // Минимальная распознаваемая нота
-        const val MAX_HZ: Double = 987.77   // Максимальная распознаваемая нота
-        const val OCTAVE_NOTES: Double = 12.0
+        const val MIN_HZ: Double = 130.81           // Минимальная распознаваемая нота
+        const val MAX_HZ: Double = 987.77           // Максимальная распознаваемая нота
+
+        const val OCTAVE_SEMITONES: Double = 12.0   // Кол-во полутонов в октаве
     }
 
     /**
+     * Запускает распознавание нот
      * @param onDetection Действия, которые следует выполнять при распознавании очередной ноты
      */
-    fun start(onDetection: (Note?) -> Unit) {
-        if (::recognizingThread.isInitialized && recognizingThread.isAlive)
-            throw IllegalStateException("Note recognizing already running")
-        else if (::recognizingThread.isInitialized && !recognizingThread.isAlive)
-            recognizingThread.start()
-        else {
-            // ^ У нас первый запуск распознавалки ---
+    suspend fun run(onDetection: (Note?) -> Unit) {
 
-            // Creates AudioDispatcher from configured default microphone,
-            // AudioDispatcher - breaks sounds into float arrays. Needs for pitch detectors, audio players, ...
-            val dispatcher = AudioDispatcherFactory.fromDefaultMicrophone(
-                sampleRate,
-                audioBufferSize,
-                bufferOverlap
-            )
+        // PitchDetectionHandler - An interface to handle detected pitch
+        val pdh = PitchDetectionHandler { result, _ ->
+            // PitchDetectionResult - A class with information about the result of a pitch detection on a block of audio,
+            // Contains:
+            // 1) pitch in Hz
+            // 2) Probability of this pitch
+            // 3) A boolean that indicates if the algorithm thinks the signal is pitched or not
+            val pitch = result.pitch.round(2)
 
-            // PitchDetectionHandler - An interface to handle detected pitch
-            val pdh = PitchDetectionHandler { result, _ ->
-                // PitchDetectionResult - A class with information about the result of a pitch detection on a block of audio,
-                // Contains:
-                // 1) pitch in Hz
-                // 2) Probability of this pitch
-                // 3) A boolean that indicates if the algorithm thinks the signal is pitched or not
-                val pitch = result.pitch.round(2)
+            // Некоторые *специальные* ограничения на распознаваемый сигнал
+            if (pitch in MIN_HZ..MAX_HZ && result.probability >= PROBABILITY_LIMIT && result.isPitched) {
+                // https://producelikeapro.com/blog/note-frequency-chart/ <-- табличка с частотами удобная
 
-                if (pitch in MIN_HZ..MAX_HZ && result.probability >= PROBABILITY_LIMIT && result.isPitched) {
-                    // https://producelikeapro.com/blog/note-frequency-chart/
+                // Кол-во полутонов, на которое сдвинута нота от MAIN_HZ
+                val relativeSemitones = round(log(pitch / START_NOTE_HZ, 2.0) * OCTAVE_SEMITONES)
+                // На какое кол-во октав смещение
+                val relativeOctave = (relativeSemitones / OCTAVE_SEMITONES).toInt()
+                // Смещение внутри октавы
+                val shiftInOctave =
+                    ((relativeSemitones - (relativeOctave * OCTAVE_SEMITONES)) / 2).round(1)
 
-                    // Кол-во полутонов, на которое сдвинута нота от MAIN_HZ
-                    val relativeSemitones = round(log(pitch / MAIN_HZ, 2.0) * OCTAVE_NOTES)
-                    // На какое кол-во октав смещение
-                    val relativeOctave = (relativeSemitones / OCTAVE_NOTES).toInt()
-                    // Смещение внутри октавы
-                    val shiftInOctave =
-                        ((relativeSemitones - (relativeOctave * OCTAVE_NOTES)) / 2).round(1)
+                Log.d("AMOGUS", "$pitch $relativeSemitones $relativeOctave $shiftInOctave")
 
-                    Log.d("AMOGUS", "$pitch $relativeSemitones $relativeOctave $shiftInOctave")
+                // Находим в октаве первую подходящую ноту
+                val noteName =
+                    NoteName.entries.firstOrNull { it.value.toDouble() >= shiftInOctave }
+                        ?: NoteName.Do
 
-                    // Находим в октаве первую подходящую ноту
-                    val noteName =
-                        NoteName.entries.firstOrNull { it.value.toDouble() >= shiftInOctave }
-                            ?: NoteName.Do
+                val note = if (noteName.value > shiftInOctave)
+                    Note(
+                        noteName,
+                        relativeOctave + OCTAVE_SHIFT
+                    ).previousSemitone() else Note(noteName, relativeOctave + OCTAVE_SHIFT)
 
-                    val note = if (noteName.value > shiftInOctave)
-                        Note(
-                            noteName,
-                            relativeOctave + MAIN_OCTAVE_SHIFT
-                        ).previousSemitone() else Note(noteName, relativeOctave + MAIN_OCTAVE_SHIFT)
-
-                    onDetection(note)
-                } else {
-                    onDetection(null)
-                }
+                onDetection(note)
+            } else {
+                onDetection(null)
             }
-
-            // AudioProcessor: Digital signal processing, a process method that works on an AudioEvent object
-            // PitchProcessor: Is responsible to call a pitch estimation algorithm
-            val p: AudioProcessor =
-                PitchProcessor(
-                    pitchEstimationAlgorithm,
-                    sampleRate.toFloat(),
-                    audioBufferSize,
-                    pdh
-                )
-            // AudioDispatcher (use micro to get sound and work with AudioProcessor)
-            // ^ AudioProcessor (More common than PitchProcessor)
-            // ^ PitchProcessor (Call pitch estimation alg + use pdh to react)
-            // ^ PitchDetectionHandler (Define how to react on pitch)
-            dispatcher.addAudioProcessor(p)
-            recognizingThread = thread { dispatcher.run() }
-
         }
+
+        // AudioProcessor: Digital signal processing, a process method that works on an AudioEvent object
+        // PitchProcessor: Is responsible to call a pitch estimation algorithm
+        val p: AudioProcessor =
+            PitchProcessor(
+                pitchEstimationAlgorithm,
+                sampleRate.toFloat(),
+                audioBufferSize,
+                pdh
+            )
+        // AudioDispatcher (use micro to get sound and work with AudioProcessor)
+        // ^ AudioProcessor (More common than PitchProcessor)
+        // ^ PitchProcessor (Call pitch estimation alg + use pdh to react)
+        // ^ PitchDetectionHandler (Define how to react on pitch)
+        dispatcher.addAudioProcessor(p)
+        dispatcher.run()
     }
 
     private fun Double.round(decimals: Int): Double {
