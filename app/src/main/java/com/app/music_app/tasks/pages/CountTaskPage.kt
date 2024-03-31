ff0277fa50e04dc122eb1e8ee7010a10e840c61b
dmitriy <st107687@student.spbu.ru>
2024-03-31 17:05:06 +0300

    Change text align

--- app/src/main/java/com/app/music_app/tasks/pages/CountTaskPage.kt
+++ app/src/main/java/com/app/music_app/tasks/pages/CountTaskPage.kt
@@ -10,6 +10,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -19,6 +20,7 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.DpSize
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -65,7 +67,12 @@ fun CountTaskPage(
         }
 
         Spacer(modifier = Modifier.fillMaxHeight(0.3f))
-        Text(text, fontSize = 30.sp)
+        Text(
+            text,
+            fontSize = 30.sp,
+            textAlign = TextAlign.Center,
+            modifier = Modifier.padding(10.dp)
+        )
         Spacer(modifier = Modifier.fillMaxHeight(0.5f))
 
         // Фортепиано
