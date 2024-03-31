f495ae2dceacdfbb3dfb84f5221cd16cdf558a41
dmitriy <st107687@student.spbu.ru>
2024-03-31 17:41:58 +0300

    Add: short names

--- app/src/main/java/com/app/music_app/names/IntervalNameResolver.kt
+++ app/src/main/java/com/app/music_app/names/IntervalNameResolver.kt
@@ -27,5 +27,25 @@ class IntervalNameResolver {
                 else -> throw IllegalStateException("Can't find name for $interval")
             }
         }
+
+        fun shortNameOf(context: Context, interval: Interval): String {
+            return when (interval) {
+                Interval(IntervalName.Prima, IntervalType.Pure) -> context.getString(R.string.pure_prima_short)
+                Interval(IntervalName.Secunda, IntervalType.Small) -> context.getString(R.string.small_secunda_short)
+                Interval(IntervalName.Secunda, IntervalType.Large) -> context.getString(R.string.large_secunda_short)
+                Interval(IntervalName.Tertia, IntervalType.Small) -> context.getString(R.string.small_tertia_short)
+                Interval(IntervalName.Tertia, IntervalType.Large) -> context.getString(R.string.large_tertia_short)
+                Interval(IntervalName.Quarta, IntervalType.Pure) -> context.getString(R.string.pure_quarta_short)
+                Interval(IntervalName.Quarta, IntervalType.Extended) -> context.getString(R.string.extended_quarta)
+                Interval(IntervalName.Quinta, IntervalType.Pure) -> context.getString(R.string.pure_quinta_short)
+                Interval(IntervalName.Sexta, IntervalType.Small) -> context.getString(R.string.small_sexta_short)
+                Interval(IntervalName.Sexta, IntervalType.Large) -> context.getString(R.string.large_sexta_short)
+                Interval(IntervalName.Septima, IntervalType.Small) -> context.getString(R.string.small_septima_short)
+                Interval(IntervalName.Septima, IntervalType.Large) -> context.getString(R.string.large_septima_short)
+                Interval(IntervalName.Octava, IntervalType.Pure) -> context.getString(R.string.pure_octave_short)
+
+                else -> throw IllegalStateException("Can't find name for $interval")
+            }
+        }
     }
 }
