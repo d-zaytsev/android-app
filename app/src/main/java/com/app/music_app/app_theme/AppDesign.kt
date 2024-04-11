c1317740a2a28e994451f16b02d2bd08d8f969c2
dmitriy <st107687@student.spbu.ru>
2024-04-11 17:29:08 +0300

    Create app theme

--- app/src/main/java/com/app/music_app/app_theme/AppDesign.kt
+++ app/src/main/java/com/app/music_app/app_theme/AppDesign.kt
@@ -1,6 +1,8 @@
 package com.app.music_app.app_theme
 
+import androidx.compose.runtime.staticCompositionLocalOf
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.Dp
@@ -34,13 +36,59 @@ data class AppSize(
 
 data class AppTypography(
     val display: TextStyle,     // Очень крупный текст
-    val titleLarge: TextStyle,  // Заголовок
+    val title: TextStyle,  // Заголовок
     val body: TextStyle,        // Основной текст
     val label: TextStyle,       // Небольшие надписи или метки
 )
 
 data class AppShape(
-    val pianoKey: Shape,        // Особая форма кнопок фортепиано
     val button: Shape,          // Для разных кнопок
     val container: Shape        // Для контейнеров (Box, Card)
 )
+
+// --- Composition local keys ---
+
+// Надо чтобы проталкивать эти данные через UI дерево
+val LocalAppColorScheme = staticCompositionLocalOf {
+    AppColorScheme(
+        surface = Color.Unspecified,
+        onSurface = Color.Unspecified,
+        primary = Color.Unspecified,
+        onPrimary = Color.Unspecified,
+        secondary = Color.Unspecified,
+        onSecondary = Color.Unspecified,
+        tertiary = Color.Unspecified,
+        onTertiary = Color.Unspecified,
+        error = Color.Unspecified,
+        onError = Color.Unspecified,
+        success = Color.Unspecified,
+        onSuccess = Color.Unspecified,
+        outline = Color.Unspecified,
+        outlineVariant = Color.Unspecified
+    )
+}
+
+val LocalAppTypography = staticCompositionLocalOf {
+    AppTypography(
+        display = TextStyle.Default,
+        title = TextStyle.Default,
+        body = TextStyle.Default,
+        label = TextStyle.Default
+    )
+}
+
+val LocalAppShape = staticCompositionLocalOf {
+    AppShape(
+        button = RectangleShape,
+        container = RectangleShape
+    )
+}
+
+val LocalAppSize = staticCompositionLocalOf {
+    AppSize(
+        large = Dp.Unspecified,
+        medium = Dp.Unspecified,
+        normal = Dp.Unspecified,
+        small = Dp.Unspecified
+    )
+}
---
+++ app/src/main/java/com/app/music_app/app_theme/AppTheme.kt
@@ -0,0 +1,122 @@
+package com.app.music_app.app_theme
+
+import androidx.compose.foundation.LocalIndication
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.ripple.rememberRipple
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.app.music_app.components.piano_keyboard.PianoKeyShape
+
+/**
+ * Впихивает значения в контекст
+ */
+@Composable
+fun AppTheme(
+    content: @Composable () -> Unit
+) {
+    val rippleIndication = rememberRipple()
+    // Проталкиваем наши значения
+    CompositionLocalProvider(
+        LocalAppColorScheme provides color,
+        LocalAppSize provides size,
+        LocalAppTypography provides typography,
+        LocalAppShape provides shape,
+        LocalIndication provides rippleIndication,
+        content = content
+    )
+}
+
+/**
+ * Позволяет обращаться к значениям (выцепляет их из контекса)
+ */
+object AppTheme {
+    val color: AppColorScheme
+        @Composable get() = LocalAppColorScheme.current
+    val size: AppSize
+        @Composable get() = LocalAppSize.current
+
+    val typography: AppTypography
+        @Composable get() = LocalAppTypography.current
+
+    val shape: AppShape
+        @Composable get() = LocalAppShape.current
+}
+
+private object AppColor {
+    // https://coolors.co/palette/03045e-0077b6-00b4d8-90e0ef-caf0f8 - App Palette
+    val CoralPink: Color
+        get() = Color(248, 131, 121)
+    val Emerald: Color
+        get() = Color(80, 200, 120)
+    val LightCyan: Color
+        get() = Color(202, 240, 248)
+    val NonPhotoBlue: Color
+        get() = Color(144, 224, 239)
+    val PacificCyan: Color
+        get() = Color(0, 180, 216)
+    val HonoluluBlue: Color
+        get() = Color(0, 119, 182)
+    val FederalBlue: Color
+        get() = Color(3, 4, 94)
+    val WhiteSmoke: Color
+        get() = Color(245, 245, 245)
+
+    val TimberWolf: Color
+        get() = Color(219, 215, 210)
+}
+
+private val color = AppColorScheme(
+    surface = AppColor.WhiteSmoke,
+    onSurface = Color.Black,
+    primary = AppColor.HonoluluBlue,
+    onPrimary = AppColor.WhiteSmoke,
+    secondary = AppColor.PacificCyan,
+    onSecondary = AppColor.LightCyan,
+    tertiary = AppColor.NonPhotoBlue,
+    onTertiary = AppColor.PacificCyan,
+    error = AppColor.CoralPink,
+    onError = AppColor.PacificCyan,
+    success = AppColor.Emerald,
+    onSuccess = AppColor.PacificCyan,
+    outline = AppColor.TimberWolf,
+    outlineVariant = AppColor.TimberWolf,
+)
+
+private val size = AppSize(
+    large = 40.dp,
+    medium = 30.dp,
+    normal = 24.dp,
+    small = 12.dp
+)
+
+private val typography = AppTypography(
+    display = TextStyle(
+        fontFamily = FontFamily.SansSerif,
+        fontWeight = FontWeight.Bold,
+        fontSize = 40.sp
+    ),
+    title = TextStyle(
+        fontFamily = FontFamily.SansSerif,
+        fontWeight = FontWeight.Bold,
+        fontSize = 30.sp
+    ),
+    body = TextStyle(
+        fontFamily = FontFamily.SansSerif,
+        fontSize = 24.sp
+    ),
+    label = TextStyle(
+        fontFamily = FontFamily.SansSerif,
+        fontSize = 12.sp
+    )
+)
+
+private val shape = AppShape(
+    button = RoundedCornerShape(10.dp),
+    container = RoundedCornerShape(15.dp)
+)
