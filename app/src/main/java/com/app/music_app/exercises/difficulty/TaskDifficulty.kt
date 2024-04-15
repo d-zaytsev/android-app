0c3c0b485c8f11e81a2f1603b551d1f36ebfc236
dmitriy <st107687@student.spbu.ru>
2024-04-15 19:44:42 +0300

    Rename & remove

--- app/src/main/java/com/app/music_app/exercises/difficulty/TaskDifficulty.kt
+++
@@ -1,6 +0,0 @@
-package com.app.music_app.exercises.difficulty
-
-/**
- * Сложность выбранного упражнения
- */
-data class TaskDifficulty(val name: String, val description: String = "", val onClick: () -> Unit)
--- app/src/main/java/com/app/music_app/exercises/difficulty/TaskDifficultyPage.kt
+++ app/src/main/java/com/app/music_app/exercises/difficulty/TaskDifficultyPage.kt
@@ -4,33 +4,28 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.Button
 import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.Card
 import androidx.compose.material.Divider
-import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import com.app.music_app.view.app_theme.AppTheme
+import com.app.music_app.view.components.CardInfo
 import com.example.android_app.R
 
 @Composable
-fun TaskDifficultyPage(difficulties: List<TaskDifficulty>, onCustomClick: (() -> Unit)? = null) {
+fun TaskDifficultyPage(difficulties: List<CardInfo>, onCustomClick: (() -> Unit)? = null) {
     Column(
         modifier = Modifier
             .fillMaxSize()
@@ -74,7 +69,7 @@ fun TaskDifficultyPage(difficulties: List<TaskDifficulty>, onCustomClick: (() ->
 }
 
 @Composable
-private fun DifficultyCard(difficulty: TaskDifficulty) {
+private fun DifficultyCard(difficulty: CardInfo) {
     Card(
         modifier = Modifier
             .fillMaxWidth(0.95f)
