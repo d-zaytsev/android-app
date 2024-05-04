70a8815a0be0a4b5f2d1c968a8628cdb78685b2d
dmitriy <st107687@student.spbu.ru>
2024-05-04 09:43:48 +0300

    save

--- app/src/main/java/com/app/music_app/MainActivity.kt
+++ app/src/main/java/com/app/music_app/MainActivity.kt
@@ -3,19 +3,42 @@ package com.app.music_app
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.DpSize
+import androidx.compose.ui.unit.dp
 import com.app.music_app.logic.exercises.builder.CompareExerciseInfo
 import com.app.music_app.view.app_theme.AppTheme
 import com.app.music_app.logic.exercises.logic.PlayIntervalExercise
 import com.app.music_app.logic.StartApp
+import com.app.music_app.logic.exercises.builder.PlayExerciseInfo
+import com.app.music_app.music_players.instruments.VirtualPiano
+import com.app.music_app.view.components.paino_box.PianoBox
+import com.app.music_app.view.components.paino_box.PianoCheckbox
+import com.app.music_app.view.components.piano_keyboard.PianoKeyboard
+import com.app.music_app.view.screens.CompareExerciseScreen
 import com.musiclib.intervals.Interval
 import com.musiclib.intervals.IntervalName
 import com.musiclib.intervals.IntervalType
+import com.musiclib.notes.Melody
+import com.musiclib.notes.MelodyNote
 import com.musiclib.notes.Note
+import com.musiclib.notes.note_metadata.Alteration
 import com.musiclib.notes.note_metadata.NoteName
 import com.musiclib.notes.range.NoteRange
+import kotlin.math.min
 
 
 class MainActivity : ComponentActivity() {
@@ -42,95 +65,32 @@ class MainActivity : ComponentActivity() {
         }
     }
 
-//    @Preview
-//    @Composable
-//    private fun TaskScreen() {
-//        val testTitle = "Title title title title title"
-//        val testDescription = "Short difficulty description short difficulty description"
-//        AppTheme {
-//            TaskDifficultyScreen(
-//                arrayOf(
-//                    DifficultyInfo(testTitle, testDescription) {},
-//                    DifficultyInfo(testTitle, testDescription) {},
-//                    DifficultyInfo(testTitle, testDescription) {},
-//                    DifficultyInfo(testTitle, testDescription) {},
-//                )
-//            ) {}
-//        }
-//    }
+    @Preview(name = "NEXUS_7", device = Devices.NEXUS_7, locale = "ru")
+    @Preview(name = "PIXEL_C", device = Devices.PIXEL_C, locale = "ru")
+    @Preview(name = "PIXEL", device = Devices.PIXEL, locale = "ru")
 
-//    @Preview(locale = "ru")
-//    @Composable
-//    private fun ProgressBar() {
-//        AppTheme {
-//            ResultsScreen(points = 3, maxPoints = 10)
-//        }
-//    }
-
-    @Preview(locale = "en")
     @Composable
-    private fun App() {
+    private fun PianoBoxPreview() {
+        val octave = NoteRange(Note(NoteName.Do), Note(NoteName.Si))
+        val melody = Melody(
+            listOf(
+                MelodyNote(Note(NoteName.Do)),
+                MelodyNote(Note(NoteName.Si)),
+                MelodyNote(Note(NoteName.Do)),
+                MelodyNote(Note(NoteName.Si))
+            )
+        )
+
+        val size = DpSize(
+            min((LocalConfiguration.current.screenWidthDp / 2), 300).dp,
+            min((LocalConfiguration.current.screenHeightDp / 7), 200).dp
+        )
+
+        val keyboard1 = PianoKeyboard(LocalContext.current, size, octave)
+        val keyboard2 = PianoKeyboard(LocalContext.current, size, octave)
+
         AppTheme {
-            StartApp(CompareExerciseInfo())
+
         }
+
     }
-
-//    @Preview1
-//    @Composable
-//    private fun CompareExercisePreview() {
-//        val task = CompareExercise(
-//            LocalContext.current,
-//            NoteRange(Note(NoteName.Do), Note(NoteName.Si, octave = 1)),
-//            5,
-//            possibleIntervals = arrayOf(
-//                Interval(IntervalName.Tertia, IntervalType.Small),
-//                Interval(IntervalName.Secunda, IntervalType.Small)
-//            )
-//        )
-//
-//        task.run()
-//    }
-
-//    @Preview
-//    @Composable
-//    private fun ProgressBarPreview() {
-//        Column(modifier = Modifier.fillMaxSize()) {
-//
-//            var points by remember { mutableFloatStateOf(0f) }
-//            var progress by remember { mutableFloatStateOf(0f) }
-//            val maxPoints = 100f
-//
-//            TaskProgressBar(points = points, progress = progress, maxProgress = maxPoints)
-//            Button(onClick = {
-//                progress += 10f
-//            }) {
-//                Text("Add progress")
-//            }
-//            Button(onClick = {
-//                points += 10f
-//            }) {
-//                Text("Add points")
-//            }
-//        }
-//    }
-
-//    CountTask(
-//                LocalContext.current,
-//                this@MainActivity,
-//                NoteRange(Note(NoteName.Do), Note(NoteName.Si)),
-//                2,
-//                10,
-//                Interval(IntervalName.Secunda, IntervalType.Small)
-//            )
-
-//    CompareTaskManager(
-//    context = LocalContext.current,
-//    range = NoteRange(Note(NoteName.Do), Note(NoteName.Mi)),
-//    chooseVariants = 2..2,
-//    taskCount = 5,
-//    fixFirstNote = true,
-//    fixDirection = true,
-//    Interval(Note(NoteName.Do), Note(NoteName.Re)),
-//    Interval(Note(NoteName.Do), Note(NoteName.Do, sign = Alteration.SharpSign))
-//    )
-}
---
+++ app/src/main/java/com/app/music_app/logic/exercises/builder/PlayExerciseInfo.kt
@@ -0,0 +1,71 @@
+package com.app.music_app.logic.exercises.builder
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalContext
+import com.app.music_app.logic.exercises.difficulty.DifficultyInfo
+import com.app.music_app.logic.exercises.logic.CompareExercise
+import com.musiclib.intervals.Interval
+import com.musiclib.intervals.IntervalName
+import com.musiclib.intervals.IntervalType
+import com.musiclib.notes.range.NoteRange
+
+class PlayExerciseInfo : ExerciseInfo {
+    override val name: String
+        get() = "Playing intervals on an instrument"
+    override val description: String
+        get() = "You need to play the intervals using your instrument"
+
+    @Composable
+    override fun buildDifficulties(): Array<DifficultyInfo> {
+        return arrayOf(
+            DifficultyInfo(
+                "Basic",
+                "Basic intervals: minor and major seconds",
+                defaultExerciseOf(
+                    intervals = arrayOf(
+                        Interval(IntervalName.Secunda, IntervalType.Small),
+                        Interval(IntervalName.Secunda, IntervalType.Large)
+                    )
+                )
+            ),
+            DifficultyInfo(
+                "Normal",
+                "Learning to recognize thirds",
+                defaultExerciseOf(
+                    intervals = arrayOf(
+                        Interval(IntervalName.Tertia, IntervalType.Small),
+                        Interval(IntervalName.Tertia, IntervalType.Large)
+                    )
+                )
+            ),
+            DifficultyInfo(
+                "Hard",
+                "Playing fourths and fifths",
+                defaultExerciseOf(
+                    intervals = arrayOf(
+                        Interval(IntervalName.Quarta, IntervalType.Extended),
+                        Interval(IntervalName.Quinta, IntervalType.Pure),
+                        Interval(IntervalName.Sexta, IntervalType.Small),
+                        Interval(IntervalName.Sexta, IntervalType.Large)
+                    )
+                )
+            )
+        )
+    }
+    companion object {
+        private val range = NoteRange(-4, 4)
+        private const val TASK_COUNT = 5
+    }
+
+    @Composable
+    private fun defaultExerciseOf(intervals: Array<Interval>): CompareExercise {
+        return CompareExercise(
+            LocalContext.current,
+            range,
+            TASK_COUNT,
+            false,
+            fixDirection = false,
+            possibleIntervals = intervals
+        )
+    }
+}
